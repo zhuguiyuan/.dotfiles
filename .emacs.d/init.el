6adef110d9983c1a57d8be55e0a1f4560c72c0a0
zhuguiyuan <951620305@qq.com>
2024-05-24 19:06:06 +0800

    添加 scala 的补全和 LSP

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,9 +6,10 @@
 ;; 自动下载并启用 use-package 插件
 (require 'package)
 (setq package-archives
-      '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-	("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
-	("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+  '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+    ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+    ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+
 (package-initialize)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -39,6 +40,7 @@
   :ensure t
   :hook (after-init . global-company-mode)
   :config
+  (setq company-minimum-prefix-length 1)
   (setq company-tooltip-align-annotations t)
   (setq company-idle-delay 0.0)
   (setq company-show-numbers t)
@@ -56,6 +58,28 @@
   :config
   ;; Remove guess indent python message
   (setq python-indent-guess-indent-offset-verbose nil))
+
+(use-package scala-mode
+  :ensure t
+  :interpreter ("scala" . scala-mode))
+
+;; Enable sbt mode for executing sbt commands
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+   (setq sbt:program-options '("-Dsbt.supershell=false")))
+
+(use-package eglot
+  :ensure t
+  ;; (optional) Automatically start metals for Scala files.
+  :hook (scala-mode . eglot-ensure))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
