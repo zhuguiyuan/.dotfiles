aa6ea578abdd0cc7a627e2fa20da8d50f52f3242
zhuguiyuan <951620305@qq.com>
2024-06-03 17:45:43 +0800

    删除用不到的 sbt 配置和注释

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -63,22 +63,8 @@
   :ensure t
   :interpreter ("scala" . scala-mode))
 
-;; Enable sbt mode for executing sbt commands
-(use-package sbt-mode
-  :commands sbt-start sbt-command
-  :config
-  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
-  ;; allows using SPACE when in the minibuffer
-  (substitute-key-definition
-   'minibuffer-complete-word
-   'self-insert-command
-   minibuffer-local-completion-map)
-   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
-   (setq sbt:program-options '("-Dsbt.supershell=false")))
-
 (use-package eglot
   :ensure t
-  ;; (optional) Automatically start metals for Scala files.
   :hook (scala-mode . eglot-ensure))
 
 (custom-set-variables
