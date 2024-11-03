e34d8c3dda4bfdd1dd330f53ee93210bfda0f676
zhuguiyuan <951620305@qq.com>
2024-11-04 01:32:47 +0800

    Update the auto-added emacs configs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -63,3 +63,15 @@
 			    company-files
 			    company-etags))))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(company which-key avy ivy evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
