a3d10dd44e23d0f336eda1f1248defea22db8c1c
zhuguiyuan <951620305@qq.com>
2024-11-04 00:45:21 +0800

    Update the emacs config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,7 @@
-;; 检查 Emacs 版本
 (let ((minver "27.1"))
   (when (version< emacs-version minver)
     (error "Your Emacs is too old -- requires v%s or higher" minver)))
 
-;; 自动下载并启用 use-package 插件
 (require 'package)
 (setq package-archives
   '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
@@ -16,25 +14,36 @@
   (package-install 'use-package))
 
 ;; 基础配置
-(electric-pair-mode t)                          ; 自动补全括号
-(global-display-line-numbers-mode)              ; 全局显示行号
-(setq-default auto-fill-function 'do-auto-fill) ; 全局自动换行
-(setq make-backup-files nil)                    ; 关闭自动备份
-
-;; 快捷键配置
-(global-set-key (kbd "C-j") nil) ; 将 C-j 作为一个前缀键
+(use-package emacs
+  :config
+  (global-display-line-numbers-mode)
+  (setq-default auto-fill-function 'do-auto-fill)
+  (setq make-backup-files nil)
+  (global-set-key (kbd "C-j") nil))
 
 ;; 一些常用插件
 (use-package evil
+  ;:load-path "/home/zhugy/.emacs.d/evil"
   :ensure t
   :init
   (setq evil-disable-insert-state-bindings t)
   :hook (after-init . evil-mode))
 
+(use-package ivy
+  :ensure t
+  :hook (after-init . ivy-mode)
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t))
+
 (use-package avy
   :ensure t
   :bind
   (("M-j" . avy-goto-char-timer)))
+
+(use-package which-key
+  :ensure t
+  :hook (after-init . which-key-mode))
 
 (use-package company
   :ensure t
@@ -53,30 +62,4 @@
 			    company-semantic
 			    company-files
 			    company-etags))))
-
-(use-package python
-  :config
-  ;; Remove guess indent python message
-  (setq python-indent-guess-indent-offset-verbose nil))
-
-(use-package scala-mode
-  :ensure t
-  :interpreter ("scala" . scala-mode))
-
-(use-package eglot
-  :ensure t
-  :hook (scala-mode . eglot-ensure))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
